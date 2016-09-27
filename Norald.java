b3c29034573dea77f554a491378b5c686f1ab188
Norald Alejo <norald.alejo@gmail.com>
2016-09-27 12:55:26 -0700

    Set name and age

---
+++ Norald.java
@@ -0,0 +1,21 @@
+package person
+
+public class Norald implements Person{
+	String name;
+	int age; 
+
+	public Norald(){
+		name = "Norald Alejo";
+		age = 22;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+
+	public int getAge();{
+		return age;
+	}
+
+}
