46c270708a821f5905e00bbaa84243e8f05176f0
Norald Alejo <norald.alejo@gmail.com>
2016-09-27 12:54:56 -0700

    Deleted methods walk and sit

--- Person.java
+++ Person.java
@@ -5,26 +5,11 @@ public interface Person{
 	/**
 	 * Returns name of person
 	 */
-	public int getName();
+	public String getName();
 
 	/**
 	 * Returns age of person
 	 */
 	public int getAge();
 
-	/**
-	 * Updates how many miles person walked in
-	 * a set time
-	 * 
-	 * @param miles Takes in the number of miles walked
-	 * @param min   Takes in how long the person has been walking
-	 */
-	public void walk(int miles, int min);
-
-	/**
-	 * Updates how long the person has sat
-	 * 
-	 * @param min Takes the amount of time person has sat down
-	 */
-	public void sit(int min)
 }
